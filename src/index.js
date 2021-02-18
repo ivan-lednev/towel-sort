1f8adff58c911375af14f910de945a8a90920e79
Ivan Liadniou <ivan_liadniou@epam.com>
2021-02-18 11:22:20 +0100

    Add sorter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (arguments.length === 0 || matrix.length === 0) {
+        return [];
+    }
+    function reverse(arr) {
+        return arr.slice().sort((l, r) => r - l);
+    }
+    return matrix.map((arr, i) => (i % 2 === 0 ? arr : reverse(arr))).flat();
+};
